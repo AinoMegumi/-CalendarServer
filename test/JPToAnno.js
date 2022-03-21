5f22fc5adc18c5af1e099905f48e00c5572a634b
AinoMegumi <aimegu2014@gmail.com>
2022-03-22 01:44:13 +0900

    add unit test of dateSplit ( #5 )

--- test/JPToAnno.js
+++ test/JPToAnno.js
@@ -14,3 +14,28 @@ test('parseStringMapToNumMap', t => {
     t.is(parseStringMapToNumMap(null), null);
     t.is(parseStringMapToNumMap(undefined), null);
 });
+
+test('dateSplit_has_delimiter', t => {
+    t.deepEqual(dateSplit('26.12.10'), { year: '26', month: '12', day:'10' });
+    t.deepEqual(dateSplit('26-12-10'), { year: '26', month: '12', day:'10' });
+    t.deepEqual(dateSplit('26/12/10'), { year: '26', month: '12', day:'10' });
+    t.deepEqual(dateSplit('26.12-10'), { year: '26', month: '12', day:'10' });
+    t.deepEqual(dateSplit('26.12/10'), { year: '26', month: '12', day:'10' });
+    t.deepEqual(dateSplit('26/12-10'), { year: '26', month: '12', day:'10' });
+    t.deepEqual(dateSplit('aa/bb/cc'), { year: 'aa', month: 'bb', day:'cc' });
+});
+
+test('dateSplit_no_delimiter', t => {
+    t.deepEqual(dateSplit('261210'), { year: '26', month: '12', day:'10' });
+    t.deepEqual(dateSplit('aabbcc'), { year: 'aa', month: 'bb', day:'cc' });
+    t.is(dateSplit('1116'), null);
+    t.deepEqual(dateSplit('61210'), { year: '6', month: '12', day:'10' });
+    t.deepEqual(dateSplit('1261210'), { year: '126', month: '12', day:'10' });
+    t.is(dateSplit('12345678'), null);
+});
+
+test('dateSplit_has_invalid_delimiter', t => {
+    t.is(dateSplit('26_12/10'), null);
+    t.is(dateSplit('26/1/2/10'), null);
+});
+
