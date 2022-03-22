b3cc76511ff4975264801183f7f8620509709903
AinoMegumi <aimegu2014@gmail.com>
2022-03-22 21:36:02 +0900

    repair DateCompareManager constructor arg ( #4 )

--- japanese_calendar_border_table.js
+++ japanese_calendar_border_table.js
@@ -5,7 +5,7 @@ class JapaneseCalendarBorderTable {
     constructor(jcalendar, alphabet, borderArr) {
         this.m_jcalendar = jcalendar;
         this.m_alphabet = alphabet;
-        this.m_border = new DateCompareManager(borderArr);
+        this.m_border = new DateCompareManager(borderArr[0], borderArr[1], borderArr[2]);
     }
 }
 
