a2d4b555d0dd283eee2c93a50480a2c48dd8ae30
AinoMegumi <aimegu2014@gmail.com>
2022-04-03 23:06:43 +0900

    add defines when module response is null ( #19 )

--- server/index.js
+++ server/index.js
@@ -5,17 +5,25 @@ const app = express();
 const Borders = [];
 
 const main = async () => {
-    app.get('/api/japanese', (req, res) => res.send(GetJapaneseCalendar(req.query.date)));
+    app.get('/api/japanese', (req, res) => {
+        const resData = GetJapaneseCalendar(req.query.date);
+        if (resData == null) return res.sendStatus(400);
+        res.send(resData);
+    });
     app.get('/api/japanese/eras', (_, res) => {
         res.send(JSON.stringify({ eras: Borders.map(b => ({ alphabet: b.m_alphabet, kanji: b.m_jcalendar })) }));
     });
     app.get('/api/japanese/border', (req, res) => {
         if (!req.query.era) return res.sendStatus(400);
-        res.sendStatus(GetBorderInfoFromEra(req.query.era));
+        const resData = GetBorderInfoFromEra(req.query.era);
+        if (resData == null) return res.sendStatus(404);
+        res.sendStatus(resData);
     });
     app.get('/api/anno_domini', (req, res) => {
         if (!req.query.date) return res.sendStatus(400);
-        res.send(JPToAnno(req.query.date));
+        const resData = JPToAnno(req.query.date);
+        if (resData == null) return res.sendStatus(404);
+        res.send(resData);
     });
     app.use(express.static('wwwroot'));
     app.use(express.json({ type: 'application/*+json' }));
