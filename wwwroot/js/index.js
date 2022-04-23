d793ad36968a24237fa413242ff8cfdaeea378c2
AinoMegumi <aimegu2014@gmail.com>
2022-04-23 21:43:50 +0900

    update index.js for new layout(included #23 fix)

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -6,23 +6,6 @@ const ready = loaded => {
     }
 };
 
-const AddOption1 = (Target, Val, View) => {
-    const opt = document.createElement('option');
-    opt.value = Val;
-    opt.innerText = View;
-    Target.appendChild(opt);
-};
-
-const AddOption = (Target, Val) => AddOption1(Target, Val, Val);
-
-const AddOptionInSectionFromLargeVal = (Target, Max, Min) => {
-    for (let i = Max; i >= Min; i--) AddOption(Target, i);
-};
-
-const AddOptionInSectionFromSmallVal = (Target, Max, Min) => {
-    for (let i = Min; i <= Max; i++) AddOption(Target, i);
-};
-
 const CopyToClipboard = data => {
     navigator.clipboard.writeText(data);
 };
@@ -43,147 +26,45 @@ const openTab = (evt, id) => {
 };
 
 const AnnoToJP = () => {
-    const Today = new Date();
-    const year = document.getElementById('anno_year');
-    const month = document.getElementById('anno_month');
-    const day = document.getElementById('anno_day');
-    const convert = document.getElementById('calc_jp');
-    const result = document.getElementById('result_jp');
-    AddOptionInSectionFromLargeVal(year, Today.getFullYear(), 1900);
-    AddOptionInSectionFromSmallVal(month, 12, 1);
-    AddOptionInSectionFromSmallVal(day, 31, 1);
-    const YearAndMonthEventListener = () => {
-        result.style.display = 'none';
-        const d = new Date(year.value, month.value, 0).getDate();
-        while (d < day.options.length) day.removeChild(day.lastChild);
-        AddOptionInSectionFromSmallVal(day, d, parseInt(day.lastChild.value) + 1);
-    };
-    year.addEventListener('change', YearAndMonthEventListener);
-    month.addEventListener('change', YearAndMonthEventListener);
-    day.addEventListener('change', () => {
-        result.style.display = 'none';
-    });
-    convert.addEventListener('click', () => {
-        const cal = parseInt(year.value) * 10000 + parseInt(month.value) * 100 + parseInt(day.value);
-        fetch('./api/japanese?date=' + cal)
+    document.getElementById('calc_jp').addEventListener('click', () => {
+        const param = { date: document.getElementById('anno_date').value };
+        const queryParam = new URLSearchParams(param);
+        fetch('./api/japanese?' + queryParam)
             .then(res => res.json())
-            .then(r => {
-                result.style.display = 'block';
-                document.getElementById('result_jp_calendar1').value = r.jcalendar;
-                document.getElementById('result_jp_year1').value = r.year;
-                document.getElementById('result_jp_month1').value = r.mon;
-                document.getElementById('result_jp_day1').value = r.day;
-                document.getElementById('result_jp_calendar2').value = r.jalphabet;
-                document.getElementById('result_jp_year2').value = r.year;
-                document.getElementById('result_jp_month2').value = r.mon;
-                document.getElementById('result_jp_day2').value = r.day;
-            });
+            .then(
+                r =>
+                    (document.getElementById('result_jp_text').value =
+                        r.era.long + r.calendar.year + '年' + r.calendar.month + '月' + r.calendar.day + '日')
+            );
     });
 };
 
 const JPToAnno = () => {
-    const result = document.getElementById('result_anno');
-    const eraList = document.getElementById('jp_era');
-    const yearList = document.getElementById('jp_year');
-    const monthList = document.getElementById('jp_month');
-    const dayList = document.getElementById('jp_day');
-    const calcButton = document.getElementById('calc_anno');
-    const ClearSelectList = Target => {
-        while (0 < Target.options.length) Target.removeChild(Target.firstChild);
-    };
-
-    const CreateMonthAndDayListFromBorderInfo = createYearList => {
-        fetch('./api/japanese/border?era=' + eraList.value)
-            .then(res => res.json())
-            .then(r => {
-                ClearSelectList(monthList);
-                ClearSelectList(dayList);
-                if (createYearList) AddOptionInSectionFromSmallVal(yearList, (r.end === undefined ? 10 : r.end.year) - r.begin.year + 1, 1);
-                if (r.begin.year < r.end.year) {
-                    AddOptionInSectionFromSmallVal(monthList, 12, r.begin.month);
-                    AddOptionInSectionFromSmallVal(
-                        dayList,
-                        new Date(r.begin.year, r.begin.month, 0).getDate(),
-                        r.begin.day
-                    );
-                } else {
-                    AddOptionInSectionFromSmallVal(monthList, r.end.month, r.begin.month);
-                    if (r.begin.month < r.begin.month)
-                        AddOptionInSectionFromSmallVal(
-                            dayList,
-                            new Date(r.begin.year, r.begin.month, 0).getDate(),
-                            r.begin.day
-                        );
-                    else AddOptionInSectionFromSmallVal(dayList, r.end.day, r.begin.day);
-                }
-            })
-            .catch(er => console.log(er));
-    };
-
     fetch('./api/japanese/eras')
         .then(res => res.json())
         .then(r => {
-            r.eras.forEach(e => {
-                AddOption1(eraList, e.alphabet, e.kanji);
+            r.eras.array.forEach(e => {
+                const item = m('option', { value: e.kanji }, { text: e.kanji });
+                m.mount(document.getElementById('jp_era'), item);
             });
-            eraList.selectedIndex = eraList.options.length - 1;
-            CreateMonthAndDayListFromBorderInfo(true);
-        })
-        .catch(er => console.log(er));
-    eraList.addEventListener('change', () => {
-        result.style.display = 'none';
-        fetch('./api/japanese/border?era=' + eraList.value)
+        });
+    document.getElementById('calc_anno').addEventListener('click', () => {
+        const toTwoDigit = val => (parseInt(val) < 10 ? '0' : '') + val;
+        const param = {
+            date:
+                document.getElementById('jp_era').value +
+                document.getElementById('jp_year') +
+                '.' +
+                toTwoDigit(document.getElementById('jp_month').value) +
+                '.' +
+                toTwoDigit(document.getElementById('jp_day').value),
+        };
+        const queryParam = new URLSearchParams(param);
+        fetch('/api/anno_domini' + queryParam)
             .then(res => res.json())
-            .then(r => {
-                const maxYear = parseInt(r.end.year);
-                while (maxYear < yearList.options.length) yearList.removeChild(yearList.lastChild);
-                AddOptionInSectionFromSmallVal(yearList, maxYear, parseInt(yearList.lastChild.value) + 1);
-                CreateMonthAndDayListFromBorderInfo(false);
-            })
-            .catch(er => console.log(er));
-    });
-    yearList.addEventListener('change', () => {
-        result.style.display = 'none';
-        fetch('./api/japanese/border?era=' + eraList.value)
-            .then(res => res.json())
-            .then(r => {
-                ClearSelectList(monthList);
-                ClearSelectList(dayList);
-                dayList.value = '';
-                const minMonth = yearList.value === 1 ? r.begin.month : 1;
-                const maxMonth = (yearList.value === r.end.year - r.begin.year + 1) ? r.end.month : 12;
-                AddOptionInSectionFromSmallVal(monthList, maxMonth, minMonth);
-            })
-            .catch(er => console.log(er));
-    });
-    monthList.addEventListener('change', () => {
-        result.style.display = 'none';
-        fetch('./api/japanese/border?era=' + eraList.value)
-            .then(res => res.json())
-            .then(r => {
-                ClearSelectList(dayList);
-                const minDay = (yearList.value === 1 && monthList.value === r.begin.month) ? r.begin.day : 1;
-                const lastDateOfMonth = new Date(yearList.value + r.begin.year - 1, monthList.value, 1);
-                lastDateOfMonth.setDate(lastDateOfMonth.getDate() - 1);
-                const maxDay = (yearList.value === r.end.year - r.begin.year + 1 && monthList.value === r.end.month) ? r.end.day : lastDateOfMonth.getDate();
-                AddOptionInSectionFromSmallVal(dayList, minDay, maxDay);
-            })
-            .catch(er => console.log(er));
-    });
-    dayList.addEventListener('change', () => (result.style.display = 'none'));
-    calcButton.addEventListener('click', () => {
-        const dateNum = parseInt(yearList.value) * 10000 + parseInt(monthList.value) * 100 + parseInt(dayList.value);
-        fetch('./api/anno_domini?date=' + eraList.value + dateNum)
-            .then(res => res.json())
-            .then(r => {
-                result.style.display = 'block';
-                document.getElementById('result_anno_year1').value = r.year;
-                document.getElementById('result_anno_month1').value = r.mon;
-                document.getElementById('result_anno_day1').value = r.day;
-                document.getElementById('result_anno_year2').value = r.year;
-                document.getElementById('result_anno_month2').value = r.mon;
-                document.getElementById('result_anno_day2').value = r.day;
-            });
+            .then(
+                r => (document.getElementById('result_jp_text').value = r.year + '年' + r.month + '月' + r.day + '日')
+            );
     });
 };
 
@@ -207,47 +88,11 @@ const SetupCopyButton = () => {
             hideMethod: 'fadeOut',
         };
         document.getElementById('copy_to_clipboard1').addEventListener('click', () => {
-            CopyToClipboard(
-                document.getElementById('result_jp_calendar1').value +
-                    document.getElementById('result_jp_year1').value +
-                    '年' +
-                    document.getElementById('result_jp_month1').value +
-                    '月' +
-                    document.getElementById('result_jp_day1').value +
-                    '日'
-            );
+            CopyToClipboard(document.getElementById('result_jp_text').value);
             toastr['success']('クリップボードにコピーしました', '成功');
         });
         document.getElementById('copy_to_clipboard2').addEventListener('click', () => {
-            CopyToClipboard(
-                document.getElementById('result_jp_calendar2').value +
-                    document.getElementById('result_jp_year2').value +
-                    '.' +
-                    document.getElementById('result_jp_month2').value +
-                    '.' +
-                    document.getElementById('result_jp_day2').value
-            );
-            toastr['success']('クリップボードにコピーしました', '成功');
-        });
-        document.getElementById('copy_to_clipboard3').addEventListener('click', () => {
-            CopyToClipboard(
-                document.getElementById('result_anno_year1').value +
-                    '年' +
-                    document.getElementById('result_anno_month1').value +
-                    '月' +
-                    document.getElementById('result_anno_day1').value +
-                    '日'
-            );
-            toastr['success']('クリップボードにコピーしました', '成功');
-        });
-        document.getElementById('copy_to_clipboard4').addEventListener('click', () => {
-            CopyToClipboard(
-                document.getElementById('result_anno_year2').value +
-                    '.' +
-                    document.getElementById('result_anno_month2').value +
-                    '.' +
-                    document.getElementById('result_anno_day2').value
-            );
+            CopyToClipboard(document.getElementById('result_anno_text').value);
             toastr['success']('クリップボードにコピーしました', '成功');
         });
     });
