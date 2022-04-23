e2275222cc1567a1e8eedf908bcd05244d9f2cbd
AinoMegumi <aimegu2014@gmail.com>
2022-04-23 17:24:18 +0900

    add endpoint's test case to get the latest era ( #21 )

--- test/server/index.js
+++ test/server/index.js
@@ -2,8 +2,14 @@ import test from 'ava';
 import { server } from '../../server/index.js';
 import request from 'supertest';
 
-test('Get borders', async t => {
+test('Get old borders', async t => {
     const res = await request(server()).get('/api/japanese/border').query({ era: '平成' }).send();
     t.is(res.status, 200);
     t.deepEqual(res.body, { begin: { year: 1989, month: 1, day: 8 }, end: { year: 2019, month: 4, day: 30 } });
+});
+
+test('Get latest borders', async t => {
+    const res = await request(server()).get('/api/japanese/border').query({ era: '令和' }).send();
+    t.is(res.status, 200);
+    t.deepEqual(res.body, { begin: { year: 2019, month: 5, day: 1 } });
 });
