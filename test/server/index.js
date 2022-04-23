0b416adfc769959b3344adb2600e03e37b90a2cb
AinoMegumi <aimegu2014@gmail.com>
2022-04-23 18:18:11 +0900

    add '/api/anno_domini' endpoint's test case ( #21 )

--- test/server/index.js
+++ test/server/index.js
@@ -13,3 +13,10 @@ test('Get latest borders', async t => {
     t.is(res.status, 200);
     t.deepEqual(res.body, { begin: { year: 2019, month: 5, day: 1 } });
 });
+
+test('Get anno domini calendar from Japanese calendar', async t => {
+    const res = await request(server()).get('/api/anno_domini').query({ date: '令和4.04.10' }).send();
+    t.is(res.status, 200);
+    t.deepEqual(res.body, { year: 2022, month: 4, day: 10 });
+});
+
