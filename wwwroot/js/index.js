3f585459482de5b4bc50790ae65e0a8ba712a1ed
AinoMegumi <aimegu2014@gmail.com>
2022-04-24 19:17:23 +0900

    replace 'simple for' on openTab method to 'for of' ( #18 )

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -12,15 +12,9 @@ const CopyToClipboard = data => {
 
 const openTab = (evt, id) => {
     const tabcontent = document.getElementsByClassName('tabcontent');
-    for (let i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = 'none';
-    }
-
+    for (const i of tabcontent) i.style.display = 'none';
     const tablinks = document.getElementsByClassName('tablinks');
-    for (let i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(' active', '');
-    }
-
+    for (const i of tablinks) i.className = i.className.replace(' active', '');
     document.getElementById(id).style.display = 'block';
     evt.currentTarget.className += ' active';
 };
